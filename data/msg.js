e9c93a986b9dd8d532d42d91086f088221e87408
Eric Barolet <eric.barolet@gmail.com>
2011-03-23 16:00:28 -0400

    Adding data folder

---
+++ data/msg.js
@@ -0,0 +1,10 @@
+{
+  "notcompatible": {
+    "fr": "Votre navigateur ne dispose pas de toutes les fonctionalités nécessaires.",
+    "en": "Your browser is not compatible."
+  },
+  "required": {
+    "fr": "Champ obligatoire",
+    "en": "Mandatory field"
+  }
+}
